ef0c4040a678b5ee173296df18936d9bbfe39515
Dipesh Bhanani <dipesh.bhanani@hotmail.com>
2021-01-21 16:36:56 +0530

    SPFx - added gulp distribute command to automate build process

--- Online/glasswall-hide-download-ext/gulpfile.js
+++ Online/glasswall-hide-download-ext/gulpfile.js
@@ -1,7 +1,22 @@
 'use strict';
 
+// check if gulp dist was called
+if (process.argv.indexOf('dist') !== -1) {
+    // add ship options to command call
+    process.argv.push('--ship');
+}
+
 const build = require('@microsoft/sp-build-web');
 
 build.addSuppression(`Warning - [sass] The local CSS class 'ms-Grid' is not camelCase and will not be type-safe.`);
+
+// add warning suppression for immediate deployment
+build.addSuppression(/Warning - Admins can make this solution available to all sites immediately/gi);
+
+// Create clean dist package
+const gulp = require('gulp');
+const gulpSequence = require('gulp-sequence');
+
+gulp.task('dist', gulpSequence('clean', 'bundle', 'package-solution'));
 
 build.initialize(require('gulp'));
