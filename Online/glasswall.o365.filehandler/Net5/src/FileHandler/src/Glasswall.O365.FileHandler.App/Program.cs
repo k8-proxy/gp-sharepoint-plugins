8372dd9e14c70a35c6c55d92d1ebf42295170ab2
LohithGN <lohith@xamariners.com>
2021-01-26 23:25:12 +0800

    Add AppInsights , refactor
    Add AppInsights , refactor

--- Online/glasswall.o365.filehandler/Net5/src/FileHandler/src/Glasswall.O365.FileHandler.App/Program.cs
+++ Online/glasswall.o365.filehandler/Net5/src/FileHandler/src/Glasswall.O365.FileHandler.App/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using Microsoft.ApplicationInsights.Extensibility;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
@@ -14,17 +15,29 @@ namespace Glasswall.O365.FileHandler
             .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
             .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", optional: true)
             .AddEnvironmentVariables()
+            .AddUserSecrets<Program>()
             .Build();
 
         public static int Main(string[] args)
         {
-            Log.Logger = new LoggerConfiguration()
+            var loggerConfig =  new LoggerConfiguration()
                 .ReadFrom.Configuration(Configuration)
                 .Enrich.FromLogContext()
                 .WriteTo.Debug()
                 .WriteTo.Console(
-                    outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj} {Properties:j}{NewLine}{Exception}")
-                .CreateLogger();
+                    outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj} {Properties:j}{NewLine}{Exception}"
+                );
+
+            if(!string.IsNullOrEmpty(Configuration.GetValue<string>("APPINSIGHTS_INSTRUMENTATIONKEY")))
+            {
+                var appInsightsKey = Configuration.GetValue<string>("APPINSIGHTS_INSTRUMENTATIONKEY");
+                var telemetryConfig = TelemetryConfiguration.CreateDefault();
+                telemetryConfig.InstrumentationKey = appInsightsKey;
+                loggerConfig.WriteTo
+                    .ApplicationInsights(telemetryConfig,TelemetryConverter.Traces);
+            }
+            
+            Log.Logger = loggerConfig.CreateLogger();
 
             try
             {
@@ -45,10 +58,10 @@ namespace Glasswall.O365.FileHandler
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                });
+                })
+                .UseSerilog();
     }
 }
