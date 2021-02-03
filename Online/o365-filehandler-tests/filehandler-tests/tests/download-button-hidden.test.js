31d1624e6d99eeb54962ca4fa795335d033d72a0
Nirasha Chathurangi <nirasha@xamariners.com>
2021-02-03 16:31:52 +0530

    Added SP Helper
    Added SP Helper

--- Online/o365-filehandler-tests/filehandler-tests/tests/download-button-hidden.test.js
+++ Online/o365-filehandler-tests/filehandler-tests/tests/download-button-hidden.test.js
@@ -1,38 +1,45 @@
 const {chromium} = require('playwright');
 const expect = require('expect');
-
+const sphelper = require('../utils/sp-helper');
+const fileToUpload = 'resources/cleaned_files/TestAutoCurImage.jpg';
 let browser;
+let context;
 let page;
 let sharepointUrl;
 
 beforeAll(async () => {
   browser = await chromium.launch({headless:false});
-  sharepointUrl = process.env.SP_URL
-  console.log(`sharepointUrl:${sharepointUrl}`);
-  console.log(`SP_SITE:${process.env.SP_SITE}`);
-  console.log(`SP_USER:${process.env.SP_USER}`);
-  console.log(`SP_USER_PWD:${process.env.SP_USER_PWD}`);
-  //Upload some file
+  context = await browser.newContext();
+
 });
 
+
 beforeEach(async () => {
-  page = await browser.newPage();
+  page = await context.newPage();
+  await sphelper.navigateToSP(page);
+  await sphelper.login(page);
+  await sphelper.uploadFile(page,fileToUpload)  
 });
+
+
 
 it('Download button is hidden in action bar', async () => {
   //Select a file
+    
   //verify action bar does not contain default download button
+
 });
 
-it('Download button is hidden in context menu', async () => {
-  //Select a file
-  //verify context menu does not contain default download button
-});
+// it('Download button is hidden in context menu', async () => {
+//   //Select a file
+//   //verify context menu does not contain default download button
+// });
 
 afterEach(async () => {
-  await page.close();
+//  delete the uploaded file
+//  await page.close();
 });
 
-afterAll(async () => {
-  await browser.close();
-});
+// afterAll(async () => {
+//   await browser.close();
+//});
