7927bcf21f94b07acaaecb8fedfdf5df80f06387
Lohith <kashyapa78@hotmail.com>
2021-02-24 16:52:02 +0800

    E2E Test (#83)
    Adding E2E Test for SPO

---
+++ Online/o365-filehandler-tests/filehandler-tests/specs/default_download_visibility_test.spec
@@ -0,0 +1,53 @@
+Default Download Visibility Test Specification
+=======================================================
+tags: default-download
+
+
+|file_name                          |
+|-----------------------------------|
+|TestAutoCurExecuteJavaScriptPDF.pdf|
+|TestAutoCurImage.jpg               |
+
+
+Validate default download button is hidden on the Action Bar
+-------------------------------------------------------------
+tags: default-download
+
+* On the choosen SharePoint site
+* Naviagte to the Documents section
+* Upload file <file_name>
+* Select file <file_name>
+* Validate Default Download Button is Hidden in Action Bar
+* Select file <file_name>
+* Delete selected file(s)
+
+Validate default download button is hidden on the Context Menu
+--------------------------------------------------------------
+tags: default-download
+
+* On the choosen SharePoint site
+* Naviagte to the Documents section
+* Upload file <file_name>
+* Select file <file_name>
+* Validate Default Download Button is Hidden in Context Menu
+* Select file <file_name>
+* Delete selected file(s)
+
+
+Validate default download button is hidden on the Preview page
+---------------------------------------------------------------
+tags: default-download
+
+* On the choosen SharePoint site
+* Naviagte to the Documents section
+* Upload file <file_name>
+* Select file <file_name>
+* Validate Default Download Button is Hidden in Preview
+* Select file <file_name>
+* Delete selected file(s)
+
+
+____________________________________
+Teardown steps are here
+
+* Go to the SharePoint home page
---
+++ Online/o365-filehandler-tests/filehandler-tests/tests/gauge_execution_hooks.js
@@ -0,0 +1,69 @@
+/* globals gauge*/
+"use strict";
+const headless = process.env.headless.toLowerCase() === 'true';
+
+const playwright = require('playwright');
+const browserType = 'chromium'; // ['chromium', 'firefox', 'webkit']
+let browser;
+let context;
+let page;
+
+// ---------------
+// Execution Hooks
+// ---------------
+
+beforeSuite(async () => {
+    browser = await playwright[browserType.toString()].launch({ headless: headless, downloadsPath: "./resources/downloaded_files" });
+});
+
+afterSuite(async () => {
+    // Closes the browser and all of its pages (if any were opened).
+    browser.close();
+});
+
+beforeSpec(async () => {
+    context = await browser.newContext();
+    page = await context.newPage();
+
+    // Login to the AUT
+    const app_endpoint = process.env.SP_URL
+    await page.goto(app_endpoint);
+    await page.waitForSelector("//input[@name='loginfmt']");
+    await page.type("//input[@name='loginfmt']", process.env.SP_USER);
+    await page.click("//input[@value='Next']");
+    await page.type("//input[@name='passwd']", process.env.SP_USER_PWD);
+    await page.click("//input[@value='Sign in']");
+    await page.click("//input[@value='Yes']");
+});
+
+afterSpec(async () => {
+    // Closes the browser context. All the pages that belong to the browser context will be closed.
+    await context.close();
+});
+
+beforeScenario(async () => {
+    gauge.dataStore.scenarioStore.put('context', context);
+    gauge.dataStore.scenarioStore.put('page', page);
+
+});
+
+afterScenario(async () => {
+
+});
+
+beforeStep(async function () {
+    // async code for before step
+});
+
+afterStep(async function () {
+    // After every step, update scenarioStore.page with the current page
+    gauge.dataStore.scenarioStore.put('context', context);
+    gauge.dataStore.scenarioStore.put('page', page);
+});
+
+gauge.customScreenshotWriter = async function () {
+    // const screenshotFilePath = path.join(process.env['gauge_screenshots_dir'], `screenshot-${process.hrtime.bigint()}.png`);
+    // await screenshot({ path: screenshotFilePath });
+    // return path.basename(screenshotFilePath);
+};
+
