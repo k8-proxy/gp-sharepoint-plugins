89f865398d886f7e51d5a6733edb58ba8468ad4a
Lohith <kashyapa78@hotmail.com>
2021-03-09 16:08:14 +0800

    refactor SPS installer ps (#93)
    refactor wsp installation script

--- SelfHosted/installer/InstallFileHandler.ps1
+++ SelfHosted/installer/InstallFileHandler.ps1
@@ -1,52 +1,42 @@
 $solutionName = "Glasswall.FileHandler.wsp"
-
 $folder = $args[0] #absolute path to folder where wsp file is located for e.g. c:\folder
-
-if($null -eq $folder)
-{
-    throw "Absolute folder path to WSP not provided."
+if($null -eq $folder) {
+    throw("Absolute folder path to WSP not provided.")
 }
-
-if (-not (Test-Path -Path $folder\$solutionName)) {
-    throw "Package not found at $folder."
+if (-not (Test-Path -Path "$folder\$solutionName")) {
+    throw("Package $solutionName not found at $folder.")
 }
 
 # Check if solution exists, remove if exists
 Write-Host "Checking if solution exists..."
-$s = Get-SPSolution -ErrorVariable err -ErrorAction SilentlyContinue -Identity $solutionName
-    
-if($err)
-{
+$s = Get-SPSolution -ErrorVariable err -ErrorAction SilentlyContinue -identity $solutionName
+if($err) {
     Write-Host "Solution does not exist..."
 }
-else
-{
+else {
     Write-Host "Solution exists in farm..."
     Write-Host "Uninstalling solution from farm ..."
     Uninstall-SPSolution -identity $solutionName -Confirm:$false
-    Write-Host ""
-    $wspSolutionForUninstall = Get-SPSolution -Identity:$solutionName
+    $wspSolutionForUninstall = Get-SPSolution -identity $solutionName
     while ($wspSolutionForUninstall.JobExists) {
         Write-Host '.' -NoNewline
-        sleep -Seconds:1
-        $wspSolutionForUninstall = Get-SPSolution -Identity:$solutionName
+        Start-Sleep -Seconds:1
+        $wspSolutionForUninstall = Get-SPSolution -identity $solutionName
     }
-
     Write-Host "`nRemoving from the farm ..."
-    Remove-SPSolution –Identity $solutionName -Confirm:$false -Force
+    Remove-SPSolution -identity $solutionName -Confirm:$false -Force
     Start-Sleep -Seconds 20
-    Write-Host "Completed"
+    Write-Host Completed
 }
 
 # Add solution
-Write-Host "Uploading solution to farm ..." 
+Write-Host Uploading solution to farm ...
 $s = Add-SPSolution $folder\$solutionName
-Write-Host "Deploying solution to the webapplications ..." 
+Write-Host Deploying solution to the webapplications ... 
 Start-Sleep -Seconds 20
-Install-SPSolution -Identity $solutionName -GACDeployment -Force
+Install-SPSolution -identity $solutionName -GACDeployment -Force
 Start-Sleep -Seconds 20
 
-Write-Host "Restarting iis ..."     
+Write-Host Restarting iis ...
 iisreset
-
-Write-Host "Completed"
+Write-Host Completed
