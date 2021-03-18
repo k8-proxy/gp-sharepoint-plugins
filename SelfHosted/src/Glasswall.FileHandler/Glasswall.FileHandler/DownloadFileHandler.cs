ff1371b964f59274ba1662d0b9d9efce7ebcf759
lohithgn <kashyapa78@hotmail.com>
2021-03-18 12:52:48 +0800

    fixed path issue for file download
    fixed path issue for file download

--- SelfHosted/src/Glasswall.FileHandler/Glasswall.FileHandler/DownloadFileHandler.cs
+++ SelfHosted/src/Glasswall.FileHandler/Glasswall.FileHandler/DownloadFileHandler.cs
@@ -35,13 +35,11 @@ namespace Glasswall.FileHandler
                 /// This is to handle the download.aspx which is triggered by Download Button
                 if (!string.IsNullOrEmpty(context.Request.QueryString["SourceUrl"]))
                 {
-                    String redirectURL = context.Request.QueryString["SourceUrl"];
-                    string docUrl = SPContext.Current.Site.Url + redirectURL;
-                    urlPath = docUrl;
+                    urlPath = BuildDocumentUrl(context);
                 }                
-                string base64 = GetFileContentAsBase64(urlPath);
                 string fileName = Path.GetFileName(urlPath);
                 GWUtility.WriteLog(" Filename: " + fileName);
+                string base64 = GetFileContentAsBase64(urlPath);
                 byte[] regeneratedFileBytes = new GlasswallRebuildClient().RebuildFile(base64);
 
                 context.Response.Clear();
@@ -57,10 +55,39 @@ namespace Glasswall.FileHandler
             catch (Exception ex)
             {
                 GWUtility.WriteLog($@"Exception: {ex.Message}\n{ex.StackTrace}");
-                context.Response.ContentType = MediaTypeNames.Text.Plain;
-                context.Response.Write($@"<b>Error downloading file</b>\n File:{Path.GetFileName(urlPath).ToLower()}\n Error:{ex.Message}");
+                context.Response.ContentType = MediaTypeNames.Text.Html;
+                context.Response.Write(PrepareErrorMarkup(urlPath, ex));
             }
         }
+
+        private static string PrepareErrorMarkup(string urlPath, Exception ex)
+        {
+            var markupBuilder = new StringBuilder();
+            markupBuilder.Append("<html>");
+            markupBuilder.Append("<head>");
+            markupBuilder.Append("<style>body{font-family: Arial, Helvetica, sans-serif;font-size: 13px;}.error{border: 1px solid;margin: 10px 0px;padding: 15px 10px 15px 50px;background-repeat: no-repeat;background-position: 10px center;color: #D8000C;background-color: #FFBABA;}</style>");
+            markupBuilder.Append("</head>");
+            markupBuilder.Append("<body>");
+            markupBuilder.Append("<div class=\"error\">");
+            markupBuilder.Append("<h3>Error downloading file</h3>" +
+                                 $"<br>File:{Path.GetFileName(urlPath).ToLower()}" +
+                                 $"<br>Error:{ex.Message}" +
+                                 $"<br><br><a href={SPContext.Current.Site.Url}>Back to site</a>");
+            return markupBuilder.ToString();
+        }
+
+        private static string BuildDocumentUrl(HttpContext context)
+        {
+            string urlPath;
+            var redirectURL = context.Request.QueryString["SourceUrl"];
+            GWUtility.WriteLog("SourceUrl ... " + redirectURL);
+            var currentSiteUrl = new Uri(SPContext.Current.Site.Url, UriKind.Absolute);
+            var spDomain = string.Format("{0}://{1}:{2}", currentSiteUrl.Scheme, currentSiteUrl.Host, currentSiteUrl.Port);
+            urlPath = spDomain + redirectURL;
+            GWUtility.WriteLog("DocumentUrl ... " + urlPath);
+            return urlPath;
+        }
+
         private string GetFileContentAsBase64(string filePath)
         {
             SPFile file = SPContext.Current.Web.GetFile(filePath);
